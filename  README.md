7d0cb097b13c468cfd650fca992f4e6a0fe2a2ae
jeanoza <choikb916@gmail.com>
2023-12-05 19:36:16 +0100

    feat: graphql configure

---
+++  README.md
@@ -0,0 +1,9 @@
+## Simple Express Server using Graphql
+
+### Why GraphQL not RestAPI?
+
+- `over-fetching` : in GraphQL client can request only data needed
+
+- `undef-fetching`: in GraphQL client send only one request for event data joined by multiple table.
+
+  cf: in Restfull API, sometimes client should send more than 2 requests to get data he want.(when there is a other table to join for example)
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
 import express, { Express, Request, Response } from "express";
+import { ApolloServer, gql } from "apollo-server";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
+/**
+ * Query: is the root of all queries that can be asked of the GraphQL server.
+ * It's mandatory to have at least one type Query in the schema.
+ * This is something like get request in REST.
+ *
+ * Mutation: is the root of all mutations that can be executed against the GraphQL server.
+ * This is something like post, put, delete request in REST.
+ *
+ * '!': means that the field is required, else it is optional.
+ */
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    firstName: String!
+    lastName: String!
+  }
+  type Tweet {
+    id: ID!
+    text: String!
+    author: User!
+  }
+  type Query {
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
+  }
+`;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+const app = new ApolloServer({ typeDefs });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+app.listen(process.env.PORT).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
